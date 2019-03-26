140bdfe65f99511a19d5bbac008a0311b237bb99
janineA <janine@ucar.edu>
2019-03-26 17:15:59 -0600

    First pass at real-time timeseries plot that updates. Doesn't scroll off the left - just compresses. X axis is index, not time. Embedded pan and zoom functions can cause you to loose the plot.

--- src/MTPviewer.py
+++ src/MTPviewer.py
@@ -12,8 +12,13 @@
 import socket
 import sys
 import PyQt5
+import pyqtgraph as pg
+from pyqtgraph.Qt import QtGui
 
 from PyQt5.QtWidgets import QApplication,QGridLayout,QMdiArea,QWidget,QMainWindow
+
+sys.path.append('.')
+from readmtp import readMTP
 
 class MainWindow(QMainWindow):
 
@@ -27,9 +32,12 @@ class MainWindow(QMainWindow):
 
 def main():
 
-    app = QApplication(sys.argv)
-    win = MainWindow()
-    win.show()
+    app = QtGui.QApplication(sys.argv)
+    win = pg.GraphicsWindow(title="MTP plots") # Creates a window
+    p1 = win.addPlot(title="SAPITCH") # Creates empty space for the plot in the win
+    saplot = p1.plot()      # Creates an empty plot
+    windowWidth = 500
+    ptr = -windowWidth
 
     # Listen for UDP packets
     udp_send_port = 32106 # from viewer to MTP
@@ -38,14 +46,30 @@ def main():
 
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.bind(("0.0.0.0", udp_read_port))
+
+    # Instantiate an instance of an MTP reader
+    reader = readMTP()
+    sapitch = []
+
+    # For each UDP packet received from the MTP, parse it into the reader data
+    # dictionary so it can be retrieved by variable name via calls like
+    # reader.getData(variableName)
     try:
         while True:
-            data = sock.recv(1024)
-            print(data)
+            data = sock.recv(1024).decode()
+            #print(data)
+            reader.parseAsciiPacket(data)
+            #print (reader.getData('SAPITCH'))
+            sapitch.append(float(reader.getData('SAPITCH')))
+            saplot.setData(sapitch)
+            saplot.setPos(ptr,0)
+            QtGui.QApplication.processEvents()
     except KeyboardInterrupt:
         interrupted = True
 
-    sys.exit(app.exec_())
+
+    pg.QtGui.QApplication.exec_()
+    sys.exit()
 
 
 if __name__ == "__main__":
