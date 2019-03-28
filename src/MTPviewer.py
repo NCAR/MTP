f40c00d81688ca9b114c7641ed4c15ee12866e86
janineA <janine@ucar.edu>
2019-03-28 14:08:19 -0600

    Total rework. Separate viewer and client into two classes. Implement QSocketNotifier. Get plot to scroll nicely from the beginning (this is a hack until I figure out the *right* way to do this with pyqtgraph. Next: Add dropdown to select what timeseries variable to plot. And add labels, etc to plot

--- src/MTPviewer.py
+++ src/MTPviewer.py
@@ -9,67 +9,141 @@
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2019
 ################################################################################
 
+import select
 import socket
 import sys
 import PyQt5
+import numpy
 import pyqtgraph as pg
 from pyqtgraph.Qt import QtGui
 
-from PyQt5.QtWidgets import QApplication,QGridLayout,QMdiArea,QWidget,QMainWindow
+from PyQt5.QtWidgets import QApplication,QGridLayout,QWidget,QPushButton
+from PyQt5.QtCore import QSocketNotifier
 
 sys.path.append('.')
 from readmtp import readMTP
 
-class MainWindow(QMainWindow):
+app = QtGui.QApplication(sys.argv)
+
+class MTPclient():
 
     def __init__(self):
-        super(MainWindow, self).__init__()
+        # Hack-y stuff to get plot to scroll. pyqtgraph must have a better way
+        # that I haven't found yet.
+        self.scanInterval = 17 # The MTP takes a scan about once every 17
+                               # seconds. Used to scroll plotting
+        self.plotWidth = 150   # 150 scans*17s=42.5 min = the width of the time
+                               # window shown on the scrolling plot
+        self.xvar = [0]*self.plotWidth    # Current X values being plotted
+        self.yvar = [numpy.nan]*self.plotWidth # Necessary to get data to scroll
+                                               # Before get good data, plot NANs
 
-        self.initUI()
+        self.udp_send_port = 32106 # from viewer to MTP
+        self.udp_read_port = 32107 # from MTP to viewer
 
-    def initUI(self):
-        self.setWindowTitle('TEST')
+
+    def connect(self):
+        # Connection to UDP data stream
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        self.sock.bind(("0.0.0.0", self.udp_read_port))
+
+    def getSocketFileDescriptor(self):
+        return self.sock.fileno()
+
+    def readSocket(self,xvar,yvar):
+        # Instantiate an instance of an MTP reader
+        reader = readMTP()
+
+        # Listen for UDP packets
+        data = self.sock.recv(1024).decode()
+
+        # Store data to data dictionary
+        reader.parseAsciiPacket(data)
+
+        # Append new X value to end of list
+        self.xvar.append(int(reader.getData(xvar)))
+
+        # First time through, populate list with fabricated X values before
+        # first X value so plot will scroll
+        if (self.xvar[0] == 0):
+            self.xvar = list(range(self.xvar[self.plotWidth]-
+                self.plotWidth*self.scanInterval,self.xvar[self.plotWidth],
+                self.scanInterval))
+
+        # Pop oldest X value off list
+        if (len(self.xvar) > self.plotWidth):
+            self.xvar.pop(0)
+
+        # Append new X value to end of list
+        self.yvar.append(float(reader.getData(yvar)))
+        # Pop oldest Y value off list
+        if (len(self.yvar) > self.plotWidth):
+            self.yvar.pop(0)
+
+        return(self.xvar,self.yvar)
+
+
+class MTPviewer():
+
+    def __init__(self,client):
+
+        self.x=[]
+        self.y=[]
+        client.connect()
+        self.initUI(client)
+
+        self.readNotifier = QSocketNotifier(
+            client.getSocketFileDescriptor(), QSocketNotifier.Read)
+        self.readNotifier.activated.connect(lambda: self.plotData(client))
+
+    def plotData(self,client):
+        (self.x,self.y) = client.readSocket('TIME','SAPITCH')
+
+        self.saplot.setData(self.x,self.y,connect="finite")
+
+        QtGui.QApplication.processEvents()
+
+
+    def initUI(self,client):
+
+        # Define top-level widget to hold everything
+        self.w = QtGui.QWidget()
+        self.layout = QtGui.QGridLayout()
+        self.w.setLayout(self.layout)
+
+        # Create a window to hold our timeseries plot
+        win = pg.GraphicsWindow()
+        win.setWindowTitle('MTP Viewer')
+
+        # Create empty space for the plot in the win
+        p1 = win.addPlot(title="SAPITCH")
+        # Create an empty plot
+        self.saplot = p1.plot(client.xvar,client.yvar)
+        # Choose plot location
+        self.layout.addWidget(win, 0, 1)
+
+        # Add a quit button
+        button = QPushButton('Quit')
+        button.clicked.connect(lambda: self.close(client))
+        self.layout.addWidget(button,0,0)
+
+        self.w.show()
+
+        #Show the window even if data are not flowing
+        QtGui.QApplication.processEvents()
+
+    def close(self,client):
+        if client.sock:
+            client.sock.close()
+        app.quit()
+
 
 def main():
 
-    app = QtGui.QApplication(sys.argv)
-    win = pg.GraphicsWindow(title="MTP plots") # Creates a window
-    p1 = win.addPlot(title="SAPITCH") # Creates empty space for the plot in the win
-    saplot = p1.plot()      # Creates an empty plot
-    windowWidth = 500
-    ptr = -windowWidth
+    client = MTPclient()
+    viewer = MTPviewer(client)
 
-    # Listen for UDP packets
-    udp_send_port = 32106 # from viewer to MTP
-    udp_read_port = 32107 # from MTP to viewer
-    #udp_ip = "127.0.0.1"
-
-    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-    sock.bind(("0.0.0.0", udp_read_port))
-
-    # Instantiate an instance of an MTP reader
-    reader = readMTP()
-    sapitch = []
-
-    # For each UDP packet received from the MTP, parse it into the reader data
-    # dictionary so it can be retrieved by variable name via calls like
-    # reader.getData(variableName)
-    try:
-        while True:
-            data = sock.recv(1024).decode()
-            #print(data)
-            reader.parseAsciiPacket(data)
-            #print (reader.getData('SAPITCH'))
-            sapitch.append(float(reader.getData('SAPITCH')))
-            saplot.setData(sapitch)
-            saplot.setPos(ptr,0)
-            QtGui.QApplication.processEvents()
-    except KeyboardInterrupt:
-        interrupted = True
-
-
-    pg.QtGui.QApplication.exec_()
-    sys.exit()
+    sys.exit( pg.QtGui.QApplication.exec_())
 
 
 if __name__ == "__main__":
