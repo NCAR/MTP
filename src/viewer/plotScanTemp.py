97720a28140f4bfe8801ebaa8a6da24c7d963746
janineA <janine@ucar.edu>
2019-11-13 16:39:20 -0700

    Add function to plot scan brightness temperature. Required generalizing array inversion fn, making x-label specific to plot type, and making some variables local

--- src/viewer/plotScanTemp.py
+++ src/viewer/plotScanTemp.py
@@ -51,22 +51,19 @@ class ScanTemp():
         i.e. {a1c1,a1c2,a1c3,a2c1,...}. Processing requires, and the final
         data are output as {c1a1,c1a2,c1a3,c1a4,...}. Invert the array here.
         """
-        self.scnt_inv = [numpy.nan]*30
+        scnt_inv = [numpy.nan]*30
         NUM_SCAN_ANGLES = 10
         NUM_CHANNELS = 3
         for j in range(NUM_SCAN_ANGLES):
             for i in range(NUM_CHANNELS):
-                self.scnt_inv[i*10+j] = int(scnt[j*3+i])
-
-    def getSCNT(self, channel):
-        """ Return the scan counts for a single channel.
-
-        Channel 1 counts are scnt_inv indices 0-9
-        Channel 2 counts are scnt_inv indices 10-19
-        Channel 3 counts are scnt_inv indices 20-29
-        """
-
-        return(self.scnt_inv[(channel-1)*10:channel*10])
+                # The scan counts are passed in as a string, so convert them
+                # to integers. The scan brightness temperatures come in as
+                # float so leave them.
+                if (isinstance(scnt[j*3+i], str)):
+                    scnt_inv[i*10+j] = int(scnt[j*3+i])
+                else:
+                    scnt_inv[i*10+j] = scnt[j*3+i]
+        return(scnt_inv)
 
     def getAngles(self):
         """ Create an array of the angles corresponding to each SCNT value """
@@ -76,9 +73,6 @@ class ScanTemp():
 
     def configureAxis(self):
         """ Configure axis labels and limits, inc. invert right axis """
-        # set limits and label for X axis
-        self.ax.set_xlabel('Counts')
-        self.ax.set_xlim(16000, 21000)
 
         # set limits and label for left Y axis
         self.ax.set_ylabel('Altitude')
@@ -88,9 +82,15 @@ class ScanTemp():
         self.axR.set_ylabel('Scan Angle')
         self.axR.set_ylim(10, 1)  # Inverted Y axis 10 -> 1
 
-    def plotDataScnt(self):
+    def plotDataScnt(self, scnt_inv):
         """
         Plot scan counts vs channel in the self.scnt plot window
+
+        This is not done in the VB code. I added this during development so
+        I would have something to plot before I added the brightness
+        temperature calcs. Keep it in case it's useful in the future.
+
+        As of Nov 13, 2019, this function is not called.
         """
 
         # Clear the plot of data, labels and formatting for the right axis
@@ -99,9 +99,41 @@ class ScanTemp():
         # Since clear removed the labels and formatting, have to add it back
         self.configureAxis()
 
+        # set limits and label for X axis specific to counts
+        self.ax.set_xlabel('Counts')
+        self.ax.set_xlim(16000, 21000)
+
         # Plot the three channel counts on the right axis
         # channel 1 is red, channel 2 is white, and channel 3 is blue
-        self.axR.plot(self.getSCNT(1), self.getAngles(), color='red')
-        self.axR.plot(self.getSCNT(2), self.getAngles(), color="grey")
-        self.axR.plot(self.getSCNT(3), self.getAngles(), color="blue")
+        # Channel 1 counts are scnt_inv indices 0-9
+        # Channel 2 counts are scnt_inv indices 10-19
+        # Channel 3 counts are scnt_inv indices 20-29
+        self.axR.plot(scnt_inv[0:10], self.getAngles(), color='red')
+        self.axR.plot(scnt_inv[10:20], self.getAngles(), color="grey")
+        self.axR.plot(scnt_inv[20:30], self.getAngles(), color="blue")
+        self.canvas.draw()
+
+    def plotTB(self, tb):
+        """
+        Plot brightness temperature vs channel in the self.scnt plot window
+        """
+
+        # Clear the plot of data, labels and formatting for the right axis
+        self.axR.clear()
+
+        # Since clear removed the labels and formatting, have to add it back
+        self.configureAxis()
+
+        # set limits and label for X axis specific to counts
+        self.ax.set_xlabel('Brightness Temperature')
+        self.ax.set_xlim(205, 275)
+
+        # Plot the three channel counts on the right axis
+        # channel 1 is red, channel 2 is white, and channel 3 is blue
+        # Channel 1 counts are scnt_inv indices 0-9
+        # Channel 2 counts are scnt_inv indices 10-19
+        # Channel 3 counts are scnt_inv indices 20-29
+        self.axR.plot(tb[0:10], self.getAngles(), color='red')
+        self.axR.plot(tb[10:20], self.getAngles(), color="grey")
+        self.axR.plot(tb[20:30], self.getAngles(), color="blue")
         self.canvas.draw()
