a291edb714eca705177249b545a208d1899abbb6
janineA <janine@ucar.edu>
2020-01-22 10:26:43 -0700

    nc2iwg1 now runs under python3 as well as python2

--- src/emulator/snd_IWG.sh
+++ src/emulator/snd_IWG.sh
@@ -1,1 +1,1 @@
-python /Users/janine/dev/aircraft_nc2iwg1/nc2iwg1.py -i DEEPWAVErf01.nc -s 1 -u True -er True
+python3 /Users/janine/dev/aircraft_nc2iwg1/nc2iwg1.py -i DEEPWAVErf01.nc -s 1 -u True -er True
