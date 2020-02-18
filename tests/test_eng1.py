4c462cd82e8a78760090d8f47d53a1ee198e7a86
janineA <janine@ucar.edu>
2020-02-18 16:36:47 -0700

    Broke test_gui up into it's sub-tests. I am getting an intermittent error when running these tests 'QEventDispatcherMac::unregisterSocketNotifier: Tried to unregister a not registered notifier'. Seems to be related tp matplotlib at qt5agg. I can find others with this issue online, but so far no solution. This only happens running unit tests. Running code works fine.
    Former-commit-id: 3ea319f7fda43924472d5995ec83033b5c21dd52

---
+++ tests/test_eng1.py
@@ -0,0 +1,86 @@
+###############################################################################
+# GUI-specific unit tests
+#
+# To run these tests:
+#     python3 -m unittest discover -s ../tests -v
+#
+# To increase debugging info (i.e. if trying to figure out test_quit issues,
+# uncomment this line:
+# import faulthandler; faulthandler.enable()
+# and then run these tests with the -Xfaulthandler option, e.g.
+#
+# python3 -Xfaulthandler -m unittest discover -s ../tests -v
+#
+# Written in Python 3
+#
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2019
+###############################################################################
+import os
+import unittest
+from PyQt5.QtWidgets import QApplication
+
+from viewer.MTPviewer import MTPviewer
+from viewer.MTPclient import MTPclient
+from util.readmtp import readMTP
+from lib.rootdir import getrootdir
+
+import sys
+import logging
+from logger.messageHandler import Logger as logger
+
+
+class TESTeng1(unittest.TestCase):
+
+    def setUp(self):
+        # Location of default ascii_parms file
+        self.ascii_parms = os.path.join(getrootdir(), 'config', 'ascii_parms')
+        self.configfile = os.path.join(getrootdir(), 'config', 'proj.yml')
+        self.stream = sys.stdout  # Send log messages to stdout
+        loglevel = logging.INFO
+        logger.initLogger(self.stream, loglevel)
+
+        self.app = QApplication([])
+
+    def test_eng1(self):
+        """ Test Engineering 1 display window shows what we expect """
+        self.client = MTPclient()
+        self.client.config(self.configfile)
+        self.viewer = MTPviewer(self.client, self.app)
+        # This first emgineering window shows the Pt line
+        # To start, the window just shows "Channel  Counts  Ohms  Temp  "
+        self.assertEqual(self.viewer.eng1.toPlainText(),
+                         "Channel\tCounts  Ohms  Temp  ")
+
+        # Send an MTP packet to the parser and confirm it gets parsed
+        # correctly
+        line = "Pt: 2165 13811 13820 03894 13415 13342 13230 14450"
+        mtp = readMTP()
+        mtp.parseLine(line)
+        values = mtp.rawscan['Ptline']['data'].split(' ')
+        mtp.assignPtvalues(values)
+        self.assertEqual(mtp.getVar('Ptline', 'TR350CNTP'), '2165')
+        self.assertEqual(mtp.getVar('Ptline', 'TTCNTRCNTP'), '13811')
+        self.assertEqual(mtp.getVar('Ptline', 'TTEDGCNTP'), '13820')
+        self.assertEqual(mtp.getVar('Ptline', 'TWINCNTP'), '03894')
+        self.assertEqual(mtp.getVar('Ptline', 'TMIXCNTP'), '13415')
+        self.assertEqual(mtp.getVar('Ptline', 'TAMPCNTP'), '13342')
+        self.assertEqual(mtp.getVar('Ptline', 'TNDCNTP'), '13230')
+        self.assertEqual(mtp.getVar('Ptline', 'TR600CNTP'), '14450')
+
+        # Then check that window displays correct values.
+        self.viewer.client.calcPt()
+        self.viewer.writeEng1()
+        self.assertEqual(self.viewer.eng1.toPlainText(),
+                         "Channel\tCounts  Ohms  Temp  \n" +
+                         "Rref 350\t02165  350.00  \n" +
+                         "Target 1\t13811  587.00  +44.73\n" +
+                         "Target 2\t13820  587.18  +44.83\n" +
+                         "Window\t03894  385.19  -58.24\n" +
+                         "Mixer\t13415  578.94  +40.56\n" +
+                         "Dblr Amp\t13342  577.45  +39.79\n" +
+                         "Noise D.\t13230  575.17  +38.61\n" +
+                         "Rref 600\t14450  600.00  ")
+
+    def tearDown(self):
+        self.viewer.close()
+        self.app.quit()
