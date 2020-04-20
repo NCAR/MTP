119c0d690b2d6d3e9dccdb57d7f02d58e9fdfe19
Catherine dewerd <cdewerd@gmail.com>
2020-04-20 09:01:45 -0600

    Add IWG test files, txt from known smooth results with VB6
    Former-commit-id: cee54a4ca66a29614be41983c72de49abc42d917

---
+++ src/ctrl/test/IWG.py
@@ -0,0 +1,18 @@
+import socket 
+import time 
+
+UDP_IP = "127.0.0.1"
+UDP_PORT = 7071
+Message = " hi there"
+
+sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
+
+
+
+f = open("IWG1_20200211_091106.txt", "r")
+f1 = f.readlines()
+for i in f1:
+    sock.sendto(i, (UDP_IP, UDP_PORT))
+    time.sleep(1)
+
+
