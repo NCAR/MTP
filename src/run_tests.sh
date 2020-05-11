295b9a40b0c5381efd5617326d4bfdb818fdb3dd
janineA <janine@ucar.edu>
2020-05-11 11:34:49 -0600

    Jenkins does not recognize -k as a pattern indicator. Change to -p which works locally and in Jenkins.
    Former-commit-id: 00b565a4698a8f2cdc53971d7586cce851d688f6

--- src/run_tests.sh
+++ src/run_tests.sh
@@ -11,13 +11,13 @@
 # To get around this, run the test_eng tests separately (since they are
 # currently the biggest culprits), eg instead of running
 # python3 -m unittest discover -s ../tests -v
-# add a -k to pattern match on which test files to run:
+# add a -p to pattern match on which test files to run:
 #
 # Written in Python 3
 #
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2020
 ###############################################################################
-python3 -m unittest discover -s ../tests -v -k test_[0-9A-Za-df-z]
-python3 -m unittest discover -s ../tests -v -k test_eng1
-python3 -m unittest discover -s ../tests -v -k test_eng2
-python3 -m unittest discover -s ../tests -v -k test_eng3
+python3 -m unittest discover -s ../tests -v -p test_[0-9A-Za-df-z]*py
+python3 -m unittest discover -s ../tests -v -p test_eng1*py
+python3 -m unittest discover -s ../tests -v -p test_eng2*py
+python3 -m unittest discover -s ../tests -v -p test_eng3*py
