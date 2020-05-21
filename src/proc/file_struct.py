ccecc70eef38c252924de9f1df078a448f393c0c
janineA <janine@ucar.edu>
2020-05-20 18:50:51 -0600

    Really commit the processing class. Forgot to add it to last commit.
    Former-commit-id: 7a7b97c4c7400e13bc3d72057f92f5add1bcf20e

---
+++ src/proc/file_struct.py
@@ -0,0 +1,13 @@
+###############################################################################
+#
+# This python dictionary stores info on post-processing
+#
+# Written in Python 3
+#
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2019
+###############################################################################
+
+data_files = {
+    'rawFile': "",
+    'ncFile': "",
+}
