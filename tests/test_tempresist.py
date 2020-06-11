a927f7f5bffc43a6e1e2bbda8908955fdb2b469f
janineA <janine@ucar.edu>
2020-06-11 16:45:45 -0600

    Fix formatting and remove import of libraries I ended out not using

--- tests/test_tempresist.py
+++ tests/test_tempresist.py
@@ -40,12 +40,12 @@ class TESTgui(unittest.TestCase):
     def test_getResistance(self):
         """ Test that sending linetype other than Ptline fails """
         self.mtp.setCalcVal('Ptline', 'TR350CNTP', 350.00, 'resistance')
-        self.assertEqual(self.mtp.getCalcVal('Ptline', 'TR350CNTP', 'resistance'),
-                         350.00)
+        self.assertEqual(self.mtp.getCalcVal('Ptline', 'TR350CNTP',
+                                             'resistance'), 350.00)
         # check that resistance set to NAN for non Ptline
         self.mtp.setCalcVal('M02line', 'ACCPCNTE', 2510, 'resistance')
         check = numpy.isnan(self.mtp.getCalcVal('M02line', 'ACCPCNTE',
-                                           'resistance'))
+                                                'resistance'))
         self.assertTrue(check)
 
     def test_getTemperature(self):
@@ -53,15 +53,15 @@ class TESTgui(unittest.TestCase):
         # check that temperature calculated correctly for other Ptline vars
         self.mtp.setCalcVal('Ptline', 'TTCNTRCNTP', 44.73, 'temperature')
         self.assertEqual("%5.2f" % self.mtp.getCalcVal('Ptline', 'TTCNTRCNTP',
-                                                  'temperature'), '44.73')
+                                                       'temperature'), '44.73')
         # check that temperature calculated correctly for M02line vars
         self.mtp.setCalcVal('M02line', 'ACCPCNTE', -0.03, 'temperature')
         self.assertEqual("%5.2f" % self.mtp.getCalcVal('M02line', 'ACCPCNTE',
-                                                  'temperature'), '-0.03')
+                                                       'temperature'), '-0.03')
         # check that temperature set to NAN for non Ptline/M02line
         self.mtp.setCalcVal('M01line', 'VM15CNTE', -15.01, 'temperature')
         check = numpy.isnan(self.mtp.getCalcVal('M01line', 'VM15CNTE',
-                                           'temperature'))
+                                                'temperature'))
         self.assertTrue(check)
 
     def tearDown(self):
