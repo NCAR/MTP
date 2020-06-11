6b95e0cfd5f778df8d20fb16b83e91dc3a0ee458
unknown <lroot@eol-ale.eol.ucar.edu>
2020-06-11 14:35:45 -0600

    Fix spike for 1 angle, after 15s iwg.

--- src/ctrl/test/IWG.py
+++ src/ctrl/test/IWG.py
@@ -12,7 +12,7 @@ sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
 f = open("IWG1_20200211_091106.txt", "r")
 f1 = f.readlines()
 for i in f1:
-    sock.sendto(i, (UDP_IP, UDP_PORT))
+    sock.sendto(str.encode(i), (UDP_IP, UDP_PORT))
     time.sleep(1)
 
 
