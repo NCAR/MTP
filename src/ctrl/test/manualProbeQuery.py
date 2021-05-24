c6637b1af5dff8581a1b090e96a9d188c613815c
unknown <lroot@eol-atom.eol.ucar.edu>
2021-05-24 06:48:06 -0600

    Add timer changes to clean up Atom

--- src/ctrl/test/manualProbeQuery.py
+++ src/ctrl/test/manualProbeQuery.py
@@ -17,15 +17,16 @@ while 1:
     query = query + '\r\n'
     serialPort.write(query.encode('ascii'))
     logging.debug("sending: %r", query.encode('ascii'))
-
     for i in range(4):
         print(i)
         #buf = serialPort.readline()
         buf = serialPort.readline()
+        logging.debug("read %r", buf)
+        '''
         if i>1:    
             query = "S\r\n"
             serialPort.write(query.encode('ascii'))
         logging.debug("read %r", buf)
-
+        '''
 
 
