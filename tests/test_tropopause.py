4b3055aec94af91273a005648169f998c8faaf6a
Peter Walsh <peterbug80228@gmail.com>
2021-06-03 13:07:55 -0600

    Redid "Includes changes requested for pull request NCAR#4" and also fixed the direction of the slash in run_tests.sh

--- tests/test_tropopause.py
+++ tests/test_tropopause.py
@@ -75,11 +75,6 @@ class TESTtropopause(unittest.TestCase):
         # Instantiate a tropopause class
         self.trop = Tropopause(self.ATP, self.NUM_RETR_LVLS)
 
-        self.maxDiff = None  # See entire diff when asserts fail
-
-        # Set environment var to indicate we are in testing mode
-        os.environ["TEST_FLAG"] = "true"
-
         # For testing, we want to capture the log messages in a buffer so we
         # can compare the log output to what we expect.
         self.stream = StringIO()  # Set output stream to buffer
@@ -129,5 +124,3 @@ class TESTtropopause(unittest.TestCase):
 
     def tearDown(self):
         logger.delHandler()
-        if "TEST_FLAG" in os.environ:
-            del os.environ['TEST_FLAG']
