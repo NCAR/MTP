fbd78e75a6fb88b38640970c4ce46c59e4f1f957
Peter Walsh <peterbug80228@gmail.com>
2021-06-03 13:01:20 -0600

    Redid "Added logger init to other test files that didn't seem to need them but also didn't seem to have them."

--- tests/test_tropopause.py
+++ tests/test_tropopause.py
@@ -18,7 +18,11 @@
 ###############################################################################
 import unittest
 import numpy
+import os
+import logging
+from io import StringIO
 from util.tropopause import Tropopause
+from EOLpython.Qlogger.messageHandler import QLogger as logger
 
 
 class TESTtropopause(unittest.TestCase):
@@ -71,6 +75,18 @@ class TESTtropopause(unittest.TestCase):
         # Instantiate a tropopause class
         self.trop = Tropopause(self.ATP, self.NUM_RETR_LVLS)
 
+        self.maxDiff = None  # See entire diff when asserts fail
+
+        # Set environment var to indicate we are in testing mode
+        os.environ["TEST_FLAG"] = "true"
+
+        # For testing, we want to capture the log messages in a buffer so we
+        # can compare the log output to what we expect.
+        self.stream = StringIO()  # Set output stream to buffer
+
+        # Instantiate a logger
+        self.log = logger.initLogger(self.stream, logging.INFO)
+
     def test_findStart(self):
         """ Find index of first retrieval above starting altitude """
         startidx = self.trop.findStart(0, 5.6)
@@ -110,3 +126,8 @@ class TESTtropopause(unittest.TestCase):
         self.assertTrue(numpy.isnan(trop))
         self.assertTrue(numpy.isnan(altctrop))
         self.assertTrue(numpy.isnan(tempctrop))
+
+    def tearDown(self):
+        logger.delHandler()
+        if "TEST_FLAG" in os.environ:
+            del os.environ['TEST_FLAG']
