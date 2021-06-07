2a71fa45798abdaf099947a4ecaed50ece403fe7
Peter Walsh <peterbug80228@gmail.com>
2021-06-07 10:18:21 -0600

    Changes to the tests specifically for MTP63
    Former-commit-id: c1fc7838ec377943a16823ba9c2a5fa47c870fc3

--- tests/test_rcf_set.py
+++ tests/test_rcf_set.py
@@ -165,6 +165,56 @@ class TESTrcfSet(unittest.TestCase):
                          '-1.63965')
         self.assertEqual(len(BestWtdRCSet['FL_RCs']['Src']), 990)
 
+    def testgetBestWeightedRCSetSort(self):
+        # Initialize with a valid RCF directory and empty filelist so get
+        # everything
+        self.rcf = RetrievalCoefficientFileSet()
+        self.rcf.getRCFs(self.Directory + "/RC")
+
+        # Verify correct selected RCF
+        ACAltKm = 8.206
+        scanBTs = [238.371, 240.351, 241.809, 243.789, 246.028, 248.06,
+                   249.414, 250.665, 252.123, 254.207, 240.948, 241.837,
+                   242.815, 244.682, 246.238, 248.06, 248.682, 249.749,
+                   250.238, 251.438, 243.099, 244.25, 245.044, 245.639,
+                   246.79, 248.06, 248.893, 249.608, 250.679, 251.433]
+        BestWtdRCSet = self.rcf.getBestWeightedRCSet(scanBTs, ACAltKm, 0.0)
+
+        # Now that a valid RC4R has been gathered, get the ID of the second
+        # best and the ID of the third best files.
+        secondBestRCFId = BestWtdRCSet['RCFArray'][1][0]
+        thirdBestRCFId = BestWtdRCSet['RCFArray'][2][0]
+        
+        # And show that the first and second are different (actually made a new
+        # dictionary instead of just copying a reference)
+        self.assertNotEqual(BestWtdRCSet['RCFId'],
+                            secondBestRCFId)
+        self.assertNotEqual(BestWtdRCSet['RCFId'],
+                            thirdBestRCFId)
+
+        # Show that the elements of the RCFArray are correctly sorted
+        # (Should maybe be in the previous test)
+        prevElem = BestWtdRCSet['RCFArray'][0][1]
+        for i in range(len(BestWtdRCSet['RCFArray'])):
+            self.assertGreaterEqual(BestWtdRCSet['RCFArray'][i][1],
+                                    prevElem)
+            prevElem = BestWtdRCSet['RCFArray'][i][1]
+
+        # Test that the values are exactly as expected
+        self.assertEqual(BestWtdRCSet['RCFId'], "NRCDG067")
+        self.assertEqual(secondBestRCFId, "NRCDE067")
+        self.assertEqual(thirdBestRCFId, "NRCDF067")
+
+        # Ensure that SumLnProb for BestWtdRCSet is equal to the
+        # SumLnProb for the first element in RCFArray
+        self.assertEqual(BestWtdRCSet['SumLnProb'], 
+                          BestWtdRCSet['RCFArray'][0][1])
+
+        # Make sure the rest of the SumLnProbs are correct
+        self.assertEqual(BestWtdRCSet['RCFArray'][0][1], 4.363823941531192)
+        self.assertEqual(BestWtdRCSet['RCFArray'][1][1], 4.72683160362011)
+        self.assertEqual(BestWtdRCSet['RCFArray'][2][1], 5.368689246096905)
+
     def tearDown(self):
         logger.delHandler()
         if "TEST_FLAG" in os.environ:
