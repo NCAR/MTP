0e01df960c208ce68198d807387fae57ca2bb37b
Catherine dewerd <cdewerd@gmail.com>
2022-02-08 15:43:12 -0700

    Fix UDP ports as described in documentation
    Former-commit-id: cce79ca825511387d89f1b39553588a414b0feec

--- src/ctrl/test/IWG.py
+++ src/ctrl/test/IWG.py
@@ -3,8 +3,10 @@ import time
 
 UDP_IP = "127.0.0.1"
 UDP_PORT = 7071
-UDP_PORT_RIC = 32106
-UDP_PORT_not_RIC = 32107
+# UDP from RIC/ground to this
+UDP_PORT_RIC = 32107
+# UDP from this out to ground
+UDP_PORT_not_RIC = 32106
 Message = " hi there"
 
 sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
