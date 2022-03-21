d317703f7de9f5554ad468a6f5a21af0c4115164
Janine Aquino <janine@ucar.edu>
2022-03-21 19:46:49 +0000

    Add desktop icon to start mtpudpd
    Former-commit-id: e3505daf2154aa2ba8650f0fd887034f508dd6c4

---
+++ windows10/MTP2UDP.bat
@@ -0,0 +1,3 @@
+@echo on
+call C:\Users\lroot\Miniconda3\Scripts\activate.bat
+C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\MTP\scripts\mtpudpd --path C:\Users\lroot\Desktop\TI3GER\RAW --host 192.168.84.2 --port 32106 --logfile C:\Users\lroot\Desktop\TI3GER\logs\mtp2udp.log
