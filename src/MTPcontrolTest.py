78f2fd65542025789dd38c406ac72238806ed44d
janineA <janine@ucar.edu>
2022-05-18 15:13:31 -0600

    Add wrapper at MTP/src level so control and viewer code can share libraries. Implement EOLpython logger.
    Former-commit-id: 7bf2ce7acb2152b8d75321012cc1aa5816c27ffb

---
+++ src/MTPcontrolTest.py
@@ -0,0 +1,107 @@
+###############################################################################
+# Top-level program to launch the MTPcontrol test applications.
+#
+# Written in Python 3
+#
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2022
+###############################################################################
+import sys
+import argparse
+import logging
+from ctrl.test.init import MTPProbeInit
+from ctrl.test.move import MTPProbeMove
+from EOLpython.Qlogger.messageHandler import QLogger as logger
+
+
+def parse_args():
+    """ Instantiate a command line argument parser """
+
+    # Define command line arguments which can be provided by users
+    parser = argparse.ArgumentParser(
+        description="Script to initialize the MTP instrument")
+    parser.add_argument(
+        '--device', type=str, default='COM6',
+        help="Device on which to receive messages from MTP instrument")
+    parser.add_argument(
+        '--debug', dest='loglevel', action='store_const',
+        const=logging.DEBUG, default=logging.INFO,
+        help="Show debug log messages")
+    parser.add_argument(
+        '--logmod', type=str, default=None, help="Limit logging to " +
+        "given module")
+
+    # Parse the command line arguments
+    args = parser.parse_args()
+
+    return(args)
+
+
+def printMenu():
+    """ List user options """
+    print("Please issue a command:")
+    print("0 = Status")
+    print("1 = Init")
+    print("2 = Move Home")
+    print("3 = Step")
+    print("9 = Probe On Check")
+    print("x = Exit")
+
+    cmdInput = sys.stdin.readline()
+    cmdInput = str(cmdInput).strip('\n')
+
+    return(cmdInput)
+
+
+def main():
+
+    # Process command line arguments.
+    args = parse_args()
+
+    # Configure logging
+    stream = sys.stdout
+    logger.initLogger(stream, args.loglevel, args.logmod)
+
+    # Move readConfig out of viewer/MTPclient to lib/readConfig and
+    # get port from there. Add --config to parse_args - JAA
+    port = 32107
+
+    init = MTPProbeInit(args, port)
+    move = MTPProbeMove(init)
+
+    probeResponding = False
+    while (1):
+
+        cmdInput = printMenu()
+
+        # Check if probe is on and responding
+        if probeResponding is False or cmdInput == '9':
+            probeResponding = init.bootCheck()
+
+        if cmdInput == '0':
+            # Print status
+            status = init.getStatus()
+            # Should check status here. What are we looking for? - JAA
+
+        elif cmdInput == '1':
+            # Initialize probe
+            init.init()
+
+        elif cmdInput == '2':
+            move.moveHome()
+
+        elif cmdInput == '3':
+            if (move.isMovePossibleFromHome(maxDebugAttempts=12,
+                                            scanStatus='potato') == 4):
+                move.initForNewLoop()
+
+                # Move hardcoded UART commands into move.py - JAA
+                echo = move.moveTo(b'U/1J0D28226J3R\r\n')
+                s = init.findChar(echo, b'@')
+                logger.printmsg('debug', "First angle, status = %r", s)
+
+        elif cmdInput == 'x':
+            exit(1)
+
+
+if __name__ == "__main__":
+    main()
