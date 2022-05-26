a1fc865c986d794eb9c87b2804dcf1753b2b30dc
janineA <janine@ucar.edu>
2022-05-26 16:13:13 -0600

    Beef up manual query for working with probe
    Former-commit-id: 9423bc315931b637e73f788159b2de9f870b4a87

--- src/ctrl/test/manualProbeQuery.py
+++ src/ctrl/test/manualProbeQuery.py
@@ -22,34 +22,54 @@ class MTPQuery():
     def query(self):
         """ query/wait which command to send from user """
         while 1:  # Loop until user types 'q'
+
+            # Print available options
+            logger.printmsg("info", "")
+            logger.printmsg("info", "Available commands are...")
+            for i in range(0, len(self.commandlist)):
+                logger.printmsg("info", str(self.commandlist[i]))
+
             query = input("What command shall we send to the probe?\n" +
-                          "Enter 'q' to quit and return to main menu\n")
+                          "Enter 'q' to quit and return to main menu\n" +
+                          "Enter 'r' to send a readline\n")
 
             # Check for q to exit this fn and return to main menu
             if query == 'q':
                 return()
 
+            # Manual read one line from the serial port (ending in /r/n)
+            if query == 'r':
+                buf = self.serialPort.readline()
+                logger.printmsg("info", "read " + str(buf))
+
             query = query + '\r\n'
+
+            if query == '\x03':  # Ctrl-C
+                self.sendCmd(query)
+                continue
+
             # Check to make sure command is a valid command, i.e.
             # exists in command dict in ctrl/lib/mtpcommand.py
             for i in range(0, len(self.commandlist)):
-                try:
-                    if query == self.commandlist[i].decode('ascii'):
-                        self.sendCmd(query)
-                except Exception:
-                    if query == self.commandlist[i]:
-                        self.sendCmd(query)
+                if query.encode('ascii') == self.commandlist[i]:
+                    print(query.encode('ascii'))
+                    print(self.commandlist[i])
+                    self.sendCmd(query)
+                    break
+
+            if i == len(self.commandlist)-1:
+                # If didn't find a valid command, warn user
+                logger.printmsg("info", "Command not in valid cmd list")
 
     def sendCmd(self, query):
         """ Send command to probe """
         query = query.encode('ascii')
-        print("Sending: " + str(query))
 
         # send command
         self.serialPort.write(query)
         logger.printmsg("info", "sending: " + str(query))
 
-        for i in range(4):  # Read up to 4 returned lines
+        for i in range(7):  # Read up to 6 returned lines
             print(i)
             buf = self.serialPort.readline()
             logger.printmsg("info", "read " + str(buf))
