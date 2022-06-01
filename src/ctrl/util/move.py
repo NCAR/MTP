6139ce81b0d211b69b1fb7ce2b90245ad0f48439
janineA <janine@ucar.edu>
2022-06-01 15:33:12 -0600

    Check for Step: without status after home command, e.g. 'Step:\r\n' indicating tried to read status before probe stopped moving. In this case, sleep to allow probe to finish move, then send home command again.

--- src/ctrl/util/move.py
+++ src/ctrl/util/move.py
@@ -5,6 +5,7 @@
 #
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2022
 ###############################################################################
+import time
 from EOLpython.Qlogger.messageHandler import QLogger as logger
 
 
@@ -65,6 +66,14 @@ class MTPProbeMove():
         cmd = self.commandDict.getCommand(home)
         self.serialPort.write(cmd)
         answerFromProbe = self.init.readEchos(5, cmd)
+        # If readEchos called before probe finished moving, get "Step:"
+        # without \xff/0@ eg status has not yet been appended to response
+        # Can't send a status to figure that out since that would be a
+        # readEchos, so instead look for "Step:\r\n" and resend
+        if answerFromProbe.find(b'Step:\r\n'):  # status missing
+            time.sleep(1)  # Give time for probe to finish moving
+            self.serialPort.write(cmd)  # Resend home command
+            answerFromProbe = self.init.readEchos(5, cmd)
 
         status = self.init.findStat(answerFromProbe)
         if status == '@':
