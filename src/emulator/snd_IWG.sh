1340330f13a8f42acad55dc10e0f65a4163ebeed
janineA <janine@ucar.edu>
2022-06-02 11:26:15 -0600

    Update to work with latest version of nc2iwg1

--- src/emulator/snd_IWG.sh
+++ src/emulator/snd_IWG.sh
@@ -1,2 +1,2 @@
 # This is now available in aircraft_nc_utils
-/Users/janine/dev/aircraft_nc_utils/nc2iwg1/nc2iwg1 -u ../../tests/test_data/DEEPWAVE/LRT/V1.2_20150609current/DEEPWAVErf01.nc -er
+/Users/janine/dev/aircraft_nc_utils/nc2iwg1/nc2iwg1 -u True ../../tests/test_data/DEEPWAVE/LRT/V1.2_20150609current/DEEPWAVErf01.nc
