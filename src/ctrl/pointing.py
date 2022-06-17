5d509daa9dbbe1941e8b7c63058e898c8fb143de
janineA <janine@ucar.edu>
2022-06-17 10:22:31 -0600

    Clean up comments and flake8 formatting.
    Former-commit-id: e2ef2e382cb314a7495d8c7da3fca076dd1cd3b0

--- src/ctrl/pointing.py
+++ src/ctrl/pointing.py
@@ -1,63 +1,46 @@
 ###############################################################################
 # Class for all the math done to calculate pointing angle
 #
-# MJ Mahoney, June 17, 2002
-# Note that the values of yaw, pitch and roll given below were based
-# on field measurements of insturment yaw A at zero elevation angle and
-# pitch-like and roll-like parameters B and T along the MTP Sensor Unit
-# edges: The measured angles in degrees were:
-#
-# ER2:     A=20.0  B=10.3  T=25.0
-# DC8:     A=15.0  B=00.0  T=08.3  
+# Adapted from:
+# https://github.com/NCAR/MTP-VB6/blob/master/MTPH_ctrl/Pointing.bas
+# (private repo)
 #
 # Written in Python 3
 #
-# COPYRIGHT:   University Corporation for Atmospheric Research, 2019
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2022
 ###############################################################################
-from math import cos, sin, atan, asin, sqrt, log, pow
-from PyQt5 import QtCore
+from math import cos, sin, atan, asin, sqrt
 from EOLpython.Qlogger.messageHandler import QLogger as logger
-from ctrl.lib.mtpcommand import MTPcommand
+
 
 class pointMTP():
-    
-    
-    def __init__(self, parent, grandparent):
-        varDict = {
-            'Cycling': False,    
-            'lastSky': -1, #readScan sets this to actual 
-        }
-        self.parent = parent
-        self.vars = varDict 
-        self.MAM = [['nan','nan','nan', 'nan'],['nan','nan','nan', 'nan'],['nan','nan','nan','nan'],['nan','nan','nan','nan']]
-        self.MAM =  self.configMAM(self.MAM)
-        self.commandDict = MTPcommand()
 
-    
+    def __init__(self, parent, grandparent):
+        self.MAM = [['nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan'],
+                    ['nan', 'nan', 'nan', 'nan'], ['nan', 'nan', 'nan', 'nan']]
+        self.MAM = self.configMAM(self.MAM)
+
     def configMAM(self, MAM):
-        """ 
-         Calculates and stores values in mam from yi,pi,ri 
+        """
+         Calculates and stores values in mam from yi,pi,ri
          Initialized MAM in Cycle
-         Note: Inst Tilt is in roll axis 
+         Note: Inst Tilt is in roll axis
                Inst Azimuth is in yaw axis
                Inst Pitch is in pitch axis, but is included in correction
-        These were converted to the U P and R values below by numerically 
-        solving equations given at URL: 
-        http://mtp.jpl.nasa.gov/notes/pointing/pointing.html
-        Note that as of 6/4/2019 this url is defunc, these 
-        equations were coppied over from the vb6 pointing.bas
-        (currently on github)
+        These were converted to the U P and R values below by numerically
+        solving equations given at URL:
+        https://archive.eol.ucar.edu/raf/instruments/MTP/www/notes/pointing/
+             pointing.html
+
+        Calculations developed by MJ Mahoney, June 17, 2002
         """
-        
-        #specific to NSF HAIPER GV
+
+        # specific to NSF HAIPER GV
         # This will change if it changes planes
-
-
-        yi = -1.600 
+        yi = -1.600
         pi = -3.576
-        ri = -0.123 
-        rpd = 0.0174532925199433    # Radians per degree = arctan(1)/45
-
+        ri = -0.123
+        rpd = 0.0174532925199433  # Radians per degree = arctan(1)/45
 
         cY = cos(yi * rpd)
         cP = cos(pi * rpd)
@@ -79,29 +62,26 @@ class pointMTP():
         return MAM
 
     def fEc(self, pitch, roll, Elevation, EmaxFlag):
-        # Pitch and roll can be either by frame (post) or instantaneous (realtime)
-        """ Needed by goAngle in cycle in move.py """
-        # Calculate commanded elevation angle needed to be at a specified
-        # elevation angle (Elevation) with respect to the horizon
-        #
-        # MJ Mahoney, November 19, 2002
-        # Enter with MTP Attitude Matrix MAM, aircraft Pitch and Roll in degrees, 
-        # and desired elevation angle wrt to horizon.
-        #
-        # Return with commanded elevation angle
-        # if Elevation = 180 degrees, the returned value will be the target position
-        # In this case the pitch and roll values are irrelevant
-        # Excel spreadsheet shows that LHS and RHS are the only 
-        # difference in solutions
-        
+        """
+        Calculate commanded elevation angle needed to be at a specified
+        elevation angle (Elevation) with respect to the horizon
 
-        print("testfECprint")
-        # Then fEc = 180#: Return
-        if Elevation == 180: 
+        Calculations developed by MJ Mahoney, November 19, 2002
+
+        Inputs: MTP Attitude Matrix MAM
+                aircraft Pitch and Roll in degrees
+                desired elevation angle wrt to horizon.
+
+        Returns: commanded elevation angle
+        """
+        # if Elevation = 180 degrees, the returned value will be the target
+        # position. In this case the pitch and roll values are irrelevant
+        if Elevation == 180:
             return 180
+
         MAM = self.MAM
 
-        rpd = atan(1) / 45#       'Radians per degree 3.14159265358979
+        rpd = atan(1) / 45  # 'Radians per degree 3.14159265358979
 
         # convert
         P = pitch * rpd
@@ -114,7 +94,8 @@ class pointMTP():
         sR = sin(R)
         sE = sin(E)
         alpha = -cR * sP * MAM[0][0] + sR * MAM[1][0] + cR * cP * MAM[2][0]
-        beta = -1 * (-cR * sP * MAM[0][2] + sR * MAM[1][2] + cR * cP * MAM[2][2])
+        beta = -1 * (-cR * sP * MAM[0][2] + sR * MAM[1][2] +
+                     cR * cP * MAM[2][2])
 
         A = alpha * alpha + beta * beta
         B = 2 * sE * beta
@@ -122,25 +103,26 @@ class pointMTP():
         Arg = B * B - 4 * A * C
 
         if alpha == 0:
-            if beta > 0: 
+            if beta > 0:
                 Ec_at_Emax = 90
             else:
                 Ec_at_Emax = -90
         else:
             Ec_at_Emax = atan(beta / alpha) / rpd
 
-        # VB6 has a less accurate, but presumably faster ASN function for arcsin
-        E_max = -asin(alpha * cos(Ec_at_Emax * rpd) + beta * sin(Ec_at_Emax * rpd))
-        Emax = E_max / rpd #Always + since it is maximum elevation angle
-        #Debug.Print alpha; beta; Ec_at_Emax; alpha * cos(Ec_at_Emax) + beta * sin(Ec_at_Emax)
+        # VB6 has less accurate, but presumably faster ASN function for arcsin
+        E_max = -asin(alpha * cos(Ec_at_Emax * rpd) +
+                      beta * sin(Ec_at_Emax * rpd))
+        Emax = E_max / rpd  # Always + since it is maximum elevation angle
+
         Ep90 = abs(-asin(beta) / rpd)
-        Em90 = -Ep90
-        E_Ec_0 = -asin(alpha) / rpd  #Elevation at which Ec=0
+        # Em90 = -Ep90
+        E_Ec_0 = -asin(alpha) / rpd  # Elevation at which Ec=0
 
         EmaxFlag = False
-        
-        # The WB aircraft option has a special case here in the VB6 code
-        if abs(Elevation) > abs(Emax): #Go to highest or lowest elevation angle possible
+
+        if abs(Elevation) > abs(Emax):
+            # Go to highest or lowest elevation angle possible
             EmaxFlag = True
             if E_Ec_0 >= 0:
                 if Elevation >= 0:
@@ -148,10 +130,10 @@ class pointMTP():
                 else:
                     fEc = Ec_at_Emax - 180
             else:
-              if Elevation >= 0:
-                  fEc = 180 + Ec_at_Emax
-              else:
-                  fEc = Ec_at_Emax
+                if Elevation >= 0:
+                    fEc = 180 + Ec_at_Emax
+                else:
+                    fEc = Ec_at_Emax
         else:
             if Arg < 0:
                 Arg = 0
@@ -159,47 +141,32 @@ class pointMTP():
                 fEc1 = asin((-B - sqrt(Arg)) / (2 * A)) / rpd
                 fEc2 = asin((-B + sqrt(Arg)) / (2 * A)) / rpd
                 if E_Ec_0 < 0:
-                    # logical equivalent of Vb6 that follows 
+                    # logical equivalent of Vb6 that follows
                     if Elevation >= E_Ec_0 and Elevation >= Ep90:
                         fEc = 180 - fEc2
-    
+
                     else:
                         fEc = fEc2
-                      
-                    ''' VB6 
-                   If Elevation < E_Ec_0 Then
-                       fEc = fEc2       '180 - fEc1
-                   Else
-                    Select Case Elevation
-                    Case Is >= Ep90
-                        fEc = 180 - fEc2
-                    Case Is >= -Emax
-                        fEc = fEc2
-                    Case Else
-                        fEc = fEc2
-                    End Select
-                  End If
-                    '''
-                  
+
                 else:
                     if Elevation >= E_Ec_0:
-                        # Vb6 - man pages suggest the last 4 lines won't be executed
+                        # Vb6 - man pages suggest the last 4 lines won't be
+                        # executed
                         '''
                       Select Case Elevation
                       Case Is >= Emax: fEc = fEc1
                       Case Else:  fEc = fEc1
                       End Select
-                      
+
                       Case Is >= Em90: fEc = fEc1
                       Case Is > -Emax: fEc = -180 - fEc1
                       Case Else: fEc = Ec_at_Emax - 180
                       End Select
                         '''
-                        #if Elevation >= Emax:
+                        # if Elevation >= Emax:
                         fEc = fEc1
-                print("in fEc, original value = " + str(Elevation) + " corrected el = " + str(fEc))
+                logger.printmsg("debug", "in fEc, original value = " +
+                                str(Elevation) + " corrected el = " + str(fEc))
                 Elevation = fEc
 
             return Elevation
-
-
