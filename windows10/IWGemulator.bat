cd757f8c76e83dbaf1c37947242479ddf85fd21e
janineA <janine@ucar.edu>
2022-07-01 14:57:17 -0600

    Updates to make windows10 icons work

--- windows10/IWGemulator.bat
+++ windows10/IWGemulator.bat
@@ -1,5 +1,5 @@
 @echo on
-call C:\Users\janine\AppData\Local\Continuum\miniconda3\Scripts\activate.bat
-cd ..\MTP\src\emulator
-C:\Users\janine\AppData\Local\Continuum\miniconda3\python.exe C:\Users\janine\aircraft_nc2iwg1\nc2iwg1.py -i DEEPWAVERF01.nc -s 1 -u True -er True
+call C:\Users\lrootMiniconda3\Scripts\activate.bat
+cd ..\src\emulator
+C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\aircraft_nc_utils\nc2iwg1\nc2iwg1 -u True ..\..\Data\NGV\DEEPWAVE\NG\DEEPWAVERF01.nc
 pause
