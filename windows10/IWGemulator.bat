3e3e2fb8997d6397b39e97cebb4302be66e004e3
janineA <janine@ucar.edu>
2022-07-06 14:39:16 -0600

    Fix typo - missing backslash
    Former-commit-id: 10a0f8361ceeb99ae9111c63bb64952dfdcb02eb

--- windows10/IWGemulator.bat
+++ windows10/IWGemulator.bat
@@ -1,5 +1,5 @@
 @echo on
-call C:\Users\lrootMiniconda3\Scripts\activate.bat
+call C:\Users\lroot\Miniconda3\Scripts\activate.bat
 cd ..\src\emulator
 C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\aircraft_nc_utils\nc2iwg1\nc2iwg1 -u True ..\..\Data\NGV\DEEPWAVE\NG\DEEPWAVERF01.nc
 pause
