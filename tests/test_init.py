4e9047ac75133748a216846a2cd1f5dd4447c6ae
janineA <janine@ucar.edu>
2022-07-14 13:19:46 -0600

    Fix for findStat and add unit test to confirm fix

--- tests/test_init.py
+++ tests/test_init.py
@@ -49,12 +49,12 @@ class TESTinit(unittest.TestCase):
 
         # Integrator not busy (integration complete)
         for status in [0, 2, 4, 6]:
-            result = MTPProbeInit.integratorBusy(status)
+            result = MTPProbeInit.integratorBusy(self, status)
             self.assertEqual(result, False)
 
         # Integrator busy
         for status in [1, 3, 5, 7]:
-            result = MTPProbeInit.integratorBusy(status)
+            result = MTPProbeInit.integratorBusy(self, status)
             self.assertEqual(result, True)
 
     def test_stepperBusy(self):
@@ -62,12 +62,12 @@ class TESTinit(unittest.TestCase):
 
         # Stepper not moving
         for status in [0, 1, 4, 5]:
-            result = MTPProbeInit.stepperBusy(status)
+            result = MTPProbeInit.stepperBusy(self, status)
             self.assertEqual(result, False)
 
         # Stepper moving
         for status in [2, 3, 6, 7]:
-            result = MTPProbeInit.stepperBusy(status)
+            result = MTPProbeInit.stepperBusy(self, status)
             self.assertEqual(result, True)
 
     def test_synthesizerBusy(self):
@@ -77,10 +77,25 @@ class TESTinit(unittest.TestCase):
 
         # Synthesizer locked (i.e. frequency stable)
         for status in [0, 1, 2, 3]:
-            result = MTPProbeInit.synthesizerBusy(status)
+            result = MTPProbeInit.synthesizerBusy(self, status)
             self.assertEqual(result, False)
 
         # Synthesizer out of lock (i.e. frequency being adjusted)
         for status in [4, 5, 6, 7]:
-            result = MTPProbeInit.synthesizerBusy(status)
+            result = MTPProbeInit.synthesizerBusy(self, status)
             self.assertEqual(result, True)
+
+    def test_findStat(self):
+        """
+        Test logic of identifying a UART status message within the returned
+        buffer
+        """
+
+        buf = b'U/1f1j256V50000R\r\nU:U/1f1j256V50000R\r\n' + \
+              b'Step:\xff/0`\r\nStep:\xff/0@\r\n'
+        status = MTPProbeInit.findStat(self, buf)
+        self.assertEqual(status, '@')
+
+        buf = b'U/1f1j256V50000R\r\nU:U/1f1j256V50000R\r\nStep:/0@\r\n'
+        status = MTPProbeInit.findStat(self, buf)
+        self.assertEqual(status, '@')
