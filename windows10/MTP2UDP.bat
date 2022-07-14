ebefd48791ec22765db6a617668839ed118b50e1
janineA <janine@ucar.edu>
2022-07-14 16:22:28 -0600

    Update to reflect what is being used during ACCLIP

--- windows10/MTP2UDP.bat
+++ windows10/MTP2UDP.bat
@@ -1,3 +1,3 @@
 @echo on
 call C:\Users\lroot\Miniconda3\Scripts\activate.bat
-C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\MTP\scripts\mtpudpd --path C:\Users\lroot\Desktop\TI3GER\RAW --host 192.168.84.2 --port 30101 --ric 32106 --logfile C:\Users\lroot\Desktop\TI3GER\logs\mtp2udp.log
+C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\MTP\scripts\mtpudpd --path C:\Users\lroot\Desktop\ACCLIP\RAW --host 192.168.84.255 --port 30101 --ric 32106 --logfile C:\Users\lroot\Desktop\ACCLIP\logs\mtp2udp.log
