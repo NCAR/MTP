929e2e693cdd941167006eafa8401f6a0a62505d
janineA <janine@ucar.edu>
2022-08-10 13:27:26 -0600

    Create stand-along dictionary to hold an IWG record and use it instead of the complete MTPrecord dictionary to hold IWG packets
    Former-commit-id: 1471426b0da027f54f35fcf5e8e669439b39b602

---
+++ src/util/IWG.py
@@ -0,0 +1,24 @@
+###############################################################################
+#
+# This python dictionary stores the content of a single IWG record
+#
+# Written in Python 3
+#
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2022
+###############################################################################
+import numpy
+
+IWGrecord = {
+    're': "^IWG1,(........T......),(.*)",
+    'found': False,
+    'date': "",
+    'asciiPacket': "",
+    'data': [],
+    'values': {'DATE': {  # IWG1 packet Date (YYYYMMDD)
+              'val': numpy.nan},
+             'TIME': {  # IWG1 packet Time (HHMMSS) converted to secs
+              'val': numpy.nan},
+             # Additional variables are created dynamically from list of
+             # vars read from ascii_parms
+             },
+}
