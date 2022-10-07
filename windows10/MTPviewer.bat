1276317034ac64dd04c7ed4fab9319714495ddb0
janineA <janine@ucar.edu>
2022-10-07 13:23:12 -0600

    Default to LAB setup in code checkout rather than a specific project on the desktop
    Former-commit-id: 953ccf063f51df1403cc49c30201e20423090f0d

--- windows10/MTPviewer.bat
+++ windows10/MTPviewer.bat
@@ -1,4 +1,4 @@
 @echo on
 call C:\Users\lroot\Miniconda3\Scripts\activate.bat
-C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\MTP\src\MTPviewer.py --rt --config=C:\Users\lroot\Desktop\ACCLIP\config\proj.yml
+C:\Users\lroot\Miniconda3\python.exe C:\Users\lroot\MTP\src\MTPviewer.py --rt --config=C:\Users\lroot\MTP\LAB\config\sample_proj.yml
 pause
