NCAR/MTP

commit f153d928a09f3614a2d0f38b4bb06bf91a38c86b

Implement status in upper right of MTPviewer GUI to show time since last scan was retrieved.

src/viewer/MTPviewer.py
@@ -8,6 +8,7 @@
 # COPYRIGHT:   University Corporation for Atmospheric Research, 2019
 ###############################################################################
 import numpy
+import datetime
 from PyQt5.QtWidgets import QMainWindow, QGridLayout, QWidget, \
         QPlainTextEdit, QFrame, QAction, QLabel, QPushButton, QGroupBox, \
         QMessageBox, QLineEdit, QInputDialog
@@ -303,12 +304,14 @@ class MTPviewer(QMainWindow):
         self.date.setReadOnly(True)
         self.layout.addWidget(self.date, 0, 5, 1, 2)
 
-        metadata = QLabel("Connected")
-        self.layout.addWidget(metadata, 0, 8, 1, 1, alignment=Qt.AlignRight)
-        metadata = QLabel("(TBD)")
-        metadata.setStyleSheet("QLabel { color: green }")
-        metadata.setFixedHeight(25)
-        self.layout.addWidget(metadata, 0, 9, 1, 1)
+        self.scanlen = QLabel("Time since last scan")
+        self.layout.addWidget(self.scanlen, 0, 8, 1, 1,
+                              alignment=Qt.AlignRight)
+        self.scanlen = QLabel("(TBD)")
+        self.scanlen.setStyleSheet("""QLabel { color: black;
+                                   background-color: lightgreen” }""")
+        self.scanlen.setFixedHeight(25)
+        self.layout.addWidget(self.scanlen, 0, 9, 1, 1)
 
         # Create a box to hold the list of brightness temps
         box = QGroupBox()
@@ -565,6 +568,9 @@ class MTPviewer(QMainWindow):
         # Display the brightness temperatures in text format
         self.writeTB(self.cell)
 
+        # Display the time since the last scan
+        self.writeScantime()
+
         # ---------- Populate scan and template plot ----------
         # Do this before begin retrieval so that if retrieval fails, use can
         # still see that counts are being collected by instrument
@@ -750,11 +756,38 @@ class MTPviewer(QMainWindow):
         """ Display the record date in the Date box at the top of the GUI """
         self.date.setPlainText(self.client.reader.getDate())
 
+    def writeScantime(self):
+        if self.viewScanIndex != 0:  # Have two records
+            # Time of current scan
+            self.client.reader.setRawscan(self.viewScanIndex)  # current line
+            timestr = self.client.reader.getTime()
+            curtime = datetime.datetime.strptime(timestr, "%H:%M:%S")
+
+            # Time of previous scan
+            self.client.reader.setRawscan(self.viewScanIndex-1)  # prev line
+            timestr = self.client.reader.getTime()
+            prevtime = datetime.datetime.strptime(timestr, "%H:%M:%S")
+
+            timedelta = (curtime - prevtime).total_seconds()
+            # Typical scan length is 17-19 seconds
+            if timedelta > 20 and timedelta < 60:  # Long scan so warn yellow
+                self.scanlen.setStyleSheet("""QLabel { color: black;
+                                       background-color: yellow” }""")
+            elif timedelta > 60:  # If no data for 60 seconds, warn red
+                self.scanlen.setStyleSheet("""QLabel { color: black;
+                                       font-weight: bold;
+                                       background-color: red” }""")
+            else:
+                self.scanlen.setStyleSheet("""QLabel { color: black;
+                                       background-color: lightgreen” }""")
+
+            self.scanlen.setText(str(abs(timedelta)))
+
     def writeData(self):
         """ Write the latest data record to the data display box """
         self.filedata.appendPlainText("")  # Space between records
 
-        # If A line is not changing (other than date), set background to red.
+        # If A line is not changing (other than date), set text color to red.
 
         if self.viewScanIndex == 0:  # First record
             self.client.reader.setRawscan(self.viewScanIndex)  # current line

src/util/readmtp.py
@@ -230,6 +230,10 @@ class readMTP:
         """ Return the date of the current A line record """
         return(self.rawscan['Aline']['values']['DATE']['val'])
 
+    def getTime(self):
+        """ Return the date and time of the current A line record """
+        return(self.rawscan['Aline']['values']['timestr']['val'])
+
     def getAline(self):
         """ Return the A line to the caller """
         # Create the Aline

commit cc5ddefe42b697fa903c3b0f22d0f1858e8dba13

Fix bug introduced in commit f153d92 writeScantime() which caused data and plot to be out of sync. Only affects view. Data and calcs are fine.
Former-commit-id: 4056a993808b3c0a7d817ad99489be4988ebafad

src/viewer/MTPviewer.py
@@ -795,12 +795,14 @@ class MTPviewer(QMainWindow):
 
             self.scanlen.setText(str(timedelta))
 
+            # Set the scan back to the current line
+            self.client.reader.setRawscan(self.viewScanIndex)  # current line
+
     def writeData(self):
         """ Write the latest data record to the data display box """
         self.filedata.appendPlainText("")  # Space between records
 
         # If A line is not changing (other than date), set text color to red.
-
         if self.viewScanIndex != 0:  # First record
             self.client.reader.setRawscan(self.viewScanIndex)  # current line
             thisAline = self.client.reader.getAline()[20:100]  # IWG section