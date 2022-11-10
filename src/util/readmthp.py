f719a629eb7a64dff203c722c8b84c16e53696da
janineA <janine@ucar.edu>
2022-11-09 17:23:56 -0700

    First pass at class to handle data from the MTHP, including unit tests and sample data. The group gives us brightness temperature data that needs to be inserted into the MTPviewer software and used to calculate profiles. As of this commit, this class has NOT been integrated into the rest of the code base.
    Former-commit-id: e8eae38621c75274fc3ecbfd7616cb4ab576a10e

---
+++ src/util/readmthp.py
@@ -0,0 +1,152 @@
+##############################################################################
+#
+# This python class is used to read in MTHP data. It currently reads in
+# brightness temperature data, e.g. (note that the actual data is comma
+# separated and has many floating point digits of accuracy. I have truncated
+# the data here and formatted it into columns to make it easier to read.
+#
+# seconds    lat (deg) lon(deg) alt(m) angle  TB(56.363) TB(57.612) TB(58.363)
+# since 
+# unixtime
+# 1651091380 19.284874 -156.069 4226.0  1     267.066806 269.287445 272.4185533
+# 1651091380 19.284874 -156.069 4226.0  2     265.714228 266.555201 266.9693227
+# 1651091380 19.284874 -156.069 4226.0  3     264.132780 266.903078 267.3884719
+# 1651091380 19.284874 -156.069 4226.0  4     263.140593 265.142902 266.1517201
+# 1651091380 19.284874 -156.069 4226.0  5     264.360006 267.333688 266.7826546
+# 1651091380 19.284874 -156.069 4226.0  6     265.610870 267.324901 263.8911341
+# 1651091380 19.284874 -156.069 4226.0  7     260.900846 263.535000 264.1123828
+# 1651091380 19.284874 -156.069 4226.0  8     260.578599 262.553308 262.2318355
+# 1651091380 19.284874 -156.069 4226.0  9     262.412694 264.907826 264.3027885
+# 1651091380 19.284874 -156.069 4226.0  10    261.022599 262.765034 262.4812719
+# 1651091380 19.284874 -156.069 4226.0  11    260.152014 261.838927 262.7636649
+# 1651091386 19.275332 -156.062 4292.3  1     258.431222 260.982892 259.7816012
+# 1651091386 19.275332 -156.062 4292.3  2     256.121395 258.486764 258.4142367
+# , etc. where the angles [1-11] are [80,55,42,25,12,0,-12,-25,-42,-55,-80]
+#
+# Written in Python 3
+#
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2022
+###############################################################################
+import copy
+import numpy
+import time
+#from util.MTP import MTPrecord
+from util.readmtp import readMTP
+from EOLpython.Qlogger.messageHandler import QLogger
+
+logger = QLogger("EOLlogger")
+
+
+class readMTHP(readMTP):
+
+#   def __init__(self):
+#       # Instantiate dictionary to hold the MTP data. curscan is the current
+#       # scan/record we are working with.
+#       self.curscan = copy.deepcopy(MTPrecord)
+
+#       # Set the scan we are working with to be the current scan. rawscan is
+#       # always the active scan being worked with. It can point to curscan if
+#       # we are collecting/processing data, or one of the flightData scans if
+#       # we are displaying data.
+#       self.rawscan = self.curscan
+
+    def readRawScan(self, raw_data_file):
+        """
+        Read in a scan (a group of lines) from an MTHP .csv file and store them
+        to a dictionary. Keep looping until have a complete scan or reach end
+        of file.
+        """
+        # Clear current record time, lat, lon, alt
+        rectime = numpy.nan
+        reclat = numpy.nan
+        reclon = numpy.nan
+        recalt = numpy.nan
+        expected_angle_index = 1
+
+        while True:
+
+            # Read in a single line
+            line = raw_data_file.readline()
+            if len(line) == 0:  # EOF
+                return False  # At EOF
+
+            # Parse out components of current line
+            self.vals = line.split(',')
+
+            # Convert time from seconds since UNIX time to UTC and save to
+            # dictionary
+            self.parseTime(self.vals[0])
+
+            # Save time of first line in current record block
+            if numpy.isnan(float(rectime)):
+                rectime = self.vals[0]
+
+            # While time doesn't change,
+            if rectime == self.vals[0]:
+                # Save lat, lon, alt to dictionary
+                reclat = self.saveLocation('SALAT', self.vals[1], reclat)
+                reclon = self.saveLocation('SALON', self.vals[2], reclon)
+                alt = float(self.vals[3])/1000.0  # meters to km
+                recalt = self.saveLocation('SAPALT', alt, recalt)
+
+                # Store brightness temperature data to dictionary
+                angle = int(self.vals[4])  # index (1 = 80, etc)
+                ch1 = self.vals[5]  # 56.363 frequency channel
+                ch2 = self.vals[6]  # 57.612 frequency channel
+                ch3 = self.vals[7]  # 58.363 frequency channel
+                if angle == 10:
+                    continue  # Skip angle 10 (-55)
+                if angle == 11:
+                    angle = 10  # Reset 
+                if angle == expected_angle_index:
+                    # Save in original order to 'tb'
+                    index = (angle - 1) * 3
+                    self.rawscan['Bline']['values']['SCNT']['tb'][index] = \
+                        float(ch1)
+                    self.rawscan['Bline']['values']['SCNT']['tb'][index + 1] = \
+                        float(ch2)
+                    self.rawscan['Bline']['values']['SCNT']['tb'][index + 2] = \
+                        float(ch3)
+
+                    # Save in inverted order to 'tbi'
+                    index = angle - 1
+                    self.rawscan['tbi'][index] = float(ch1)
+                    self.rawscan['tbi'][index + 10] = float(ch2)
+                    self.rawscan['tbi'][index + 20] = float(ch3)
+                else:
+                    logger.info("Angle %s was not expected. Should be %d" % \
+                        (angle, expected_angle_index))
+                expected_angle_index = expected_angle_index + 1
+
+
+            # Check if we have a complete scan (all angles have found = True
+            if angle == 10:
+                # Confirm there are no nans left in 'tbi' record
+                # NANs are actually valid in MTHP scans
+                #for tbval in self.rawscan['Bline']['values']['SCNT']['tbi']:
+                #    if numpy.isnan(tbval):
+                #        logger.warning("Incomplete MTHP scan at time %s (epoch time %s" % \
+                        #            (self.rawscan['Aline']['values']['timestr']['val']), self.vals[0])
+
+                return True
+
+    def saveLocation(self, var, val, expected_val):
+        # Modifies expected_val
+        if numpy.isnan(float(expected_val)):
+            expected_val = val
+            self.rawscan['Aline']['values'][var]['val'] =  val
+        else:
+            if expected_val != val:  # error in input data
+                logger.info("%s %f varies in single scan" % (var, val))
+
+        return(expected_val)
+
+    def parseTime(self, epochtime):
+        # Convert time from seconds since UNIX time to UTC
+        utctime = time.gmtime(float(epochtime))
+        self.rawscan['Aline']['values']['DATE']['val'] = "%04d%02d%02d" % \
+            (utctime.tm_year, utctime.tm_mon, utctime.tm_mday)
+        self.rawscan['Aline']['values']['timestr']['val'] = "%02d:%02d:%02d" \
+            % (utctime.tm_hour, utctime.tm_min, utctime.tm_sec)
+        self.rawscan['Aline']['values']['TIME']['val'] = \
+            int(utctime.tm_hour * 3600 + utctime.tm_min * 60 + utctime.tm_sec)
---
+++ tests/test_readmthp.py
@@ -0,0 +1,113 @@
+###############################################################################
+# readMTHP-specific unit tests
+#
+# To run these tests:
+#     cd src/
+#     python3 -m unittest discover -s ../tests -v
+#
+# To increase debugging info (i.e. if trying to figure out test_quit issues,
+# uncomment this line:
+# import faulthandler; faulthandler.enable()
+# and then run these tests with the -Xfaulthandler option, e.g.
+#
+# python3 -Xfaulthandler -m unittest discover -s ../tests -v
+#
+# Written in Python 3
+#
+# COPYRIGHT:   University Corporation for Atmospheric Research, 2019
+###############################################################################
+import os
+import copy
+import numpy
+import unittest
+import logging
+from io import StringIO
+from unittest.mock import mock_open, patch
+from util.readmthp import readMTHP
+from lib.rootdir import getrootdir
+from pathlib import Path
+from EOLpython.Qlogger.messageHandler import QLogger
+
+logger = QLogger("EOLlogger")
+
+
+class TESTreadmthp(unittest.TestCase):
+
+    def setUp(self):
+
+        self.mthp = readMTHP()
+
+        # For testing, we want to capture the log messages in a buffer so we
+        # can compare the log output to what we expect.
+        self.stream = StringIO()  # Set output stream to buffer
+
+        # Instantiate a logger
+        self.log = logger.initStream(self.stream, logging.INFO)
+
+        # selectedRawFile is the Raw file listed in the Production dir setup
+        # file for the flight the user selected. Hardcode it here for testing
+        selectedRawFile = os.path.join(getrootdir(), 'Data', 'NGV', 'TI3GER',
+                                       'TB', 'mthp_rf8_mtp_format.csv')
+        with patch('__main__.open', mock_open()):
+            with open(selectedRawFile) as raw_data_file:
+                self.status = self.mthp.readRawScan(raw_data_file)
+
+    def test_readRawScan(self):
+        """
+        Test reading a scan from an MTHP Raw data file and storing them to a
+        dictionary
+        """
+        self.assertEqual(self.status, True)  # Successful read raw data record
+
+        # Test line parsing
+        self.assertEqual(len(self.mthp.vals), 8)  # Line contains expected vals
+        self.assertEqual(self.mthp.vals[0], '1651091379.65941')  # time
+        self.assertEqual(self.mthp.vals[1], '19.2848747461705')  # latitiude
+        self.assertEqual(self.mthp.vals[2], '-156.069267675836')  # longitude
+        self.assertEqual(self.mthp.vals[3], '4226.08404835989')  # altitude (m)
+        self.assertEqual(self.mthp.vals[4], '11')  # angle index after read
+        self.assertEqual(self.mthp.vals[5], '260.152014206036')  # ch1 @ ang 11
+        self.assertEqual(self.mthp.vals[6], '261.83892732092')  # ch2 @ ang 11
+        self.assertEqual(self.mthp.vals[7].strip(), '262.763664928933')  # ch3
+
+        self.assertEqual(self.mthp.rawscan['Bline']['values']['SCNT']['tb'], \
+            [267.066805963801, 269.287445529263, 272.418553255765, \
+            265.714228578662, 266.555201844746, 266.96932272842, \
+            264.132780586534, 266.903078826458, 267.388471872363, \
+            263.140593219671, 265.142902361059, 266.151720117981, \
+            264.360006740239, 267.333688102535, 266.782654597477, \
+            265.610870369357, 267.324901185195, 263.891134130252, \
+            260.900846490533, 263.535000033651, 264.112382820101, \
+            260.578599810011, 262.553308657995, 262.231835451778, \
+            262.412694293766, 264.907826064686, 264.302788487846, \
+            260.152014206036, 261.83892732092, 262.763664928933])
+
+    def test_parseTime(self):
+        """ Test conversion from epoch time to UTC """
+        self.mthp.parseTime(self.mthp.vals[0])
+        self.assertEqual(self.mthp.rawscan['Aline']['values']['DATE']['val'], \
+            "20220427")
+        self.assertEqual(
+            self.mthp.rawscan['Aline']['values']['timestr']['val'], "20:29:39")
+        self.assertEqual(
+            self.mthp.rawscan['Aline']['values']['TIME']['val'], 73779)
+
+    def test_saveLocation(self):
+        """ Test saving lat, lon, alt to dictionary """
+        salat = self.mthp.saveLocation('SALAT', '19.2848747461705', numpy.nan)
+        self.assertEqual(salat, '19.2848747461705')
+        self.assertEqual(self.mthp.rawscan['Aline']['values']['SALAT']['val'],
+                         '19.2848747461705')
+
+        salon = self.mthp.saveLocation('SALON', '-156.069267675836', numpy.nan)
+        self.assertEqual(salon, '-156.069267675836')
+        self.assertEqual(self.mthp.rawscan['Aline']['values']['SALON']['val'],
+                         '-156.069267675836')
+
+        saalt = self.mthp.saveLocation('SAPALT', '4226.08404835989', numpy.nan)
+        self.assertEqual(saalt, '4226.08404835989')
+        self.assertEqual(self.mthp.rawscan['Aline']['values']['SAPALT']['val'],
+                         '4226.08404835989')
+
+    def tearDown(self):
+        logger.delHandler()
